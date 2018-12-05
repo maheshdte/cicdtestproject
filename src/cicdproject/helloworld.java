09595b921dc4f5fc42d446afe7eccfe43fdb5042
Mahesh Dasika <mdasika@asiapac.globalcsc.net>
2018-12-05 23:29:30 +0530

    removed unnecessary comments

--- src/cicdproject/helloworld.java
+++ src/cicdproject/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//added comment test
+
 		System.out.println("Hello World!");
 		System.out.println("Test");
 		
